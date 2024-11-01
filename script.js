e0584ed053b7c22664c1895f30106628d11204b0
dune123 <96974209+dune123@users.noreply.github.com>
2024-11-01 13:40:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const pValue=document.getElementById('valueP');
+
+const button=document.getElementById('buttonValue');
+
+button.addEventListener('click',()=>{
+
+    pValue.innerText='Entered Metaverse';
+})
+
+
+button.addEventListener('mouseover', () => {
+    button.style.backgroundColor = '#ADD8E6'; 
+});
+
+button.addEventListener('mouseout', () => {
+    button.style.backgroundColor = ''; 
+})
